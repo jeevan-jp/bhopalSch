ff4e82f6891fb6b1485e249f65369c2fc404c602
jatingoyal06 <jgoyal1996@gmail.com>
2018-04-21 12:53:01 +0530

    refined home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,10 +14,15 @@ import { flyInOut, expand } from '../../animations/app.animations';
 export class HomeComponent implements OnInit {
   constructor( private webService: WebService ) {}
   complaints:Complaint[];
+  five: Complaint[];
   async ngOnInit() {
     const response = await this.webService.getMessages();
     console.log(response.json());
+
+
     this.complaints = response.json();
+    this.five = this.complaints.slice(this.complaints.length - 5);
+    console.log(this.five);
   }
 
 }
