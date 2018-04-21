62e49e724fc462f0d57b2fa7020ab049b401ca6b
mohitj27 <mjain8156@gmail.com>
2018-04-21 11:55:43 +0530

    Add adminview component

---
+++ src/app/adminview/adminview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { WebService } from './../web.service';
+@Component({
+  selector: 'app-adminview',
+  templateUrl: './adminview.component.html',
+  styleUrls: ['./adminview.component.css']
+})
+export class AdminviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
